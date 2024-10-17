e46f4b09bb7c4fb683f12bbcb4fe8d2fe94ea00e
catherinewuyep-at-901044882194 <AIk1wnTTmD4HJphZCOBvtgdYTEeDf+ujxTxnBMUFaM1n0PbUQ1yT0BNfnNc=>
2024-10-17 16:51:31 +0100

    user Crud and login

---
+++ routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const User = require("../models/usermodel");
+const isLoggedIn = require("../middlewares/isLoggedIn");
+const router = express.Router();
+
+const {
+    getUsers,
+    getUserProfile,
+    registerUser,
+    loginUser,
+    updateById,
+    deleteUsers,
+} = require("../controllers/userController");
+
+
+router.get("/", getUsers);
+router.get("/:id", isLoggedIn, getUserProfile);
+router.post("/signup", registerUser);
+router.post("/login", loginUser);
+router.put("/update/:id", isLoggedIn, updateById)
+router.delete("/delete/:id", isLoggedIn, deleteUsers);
+
+
+module.exports = router;
